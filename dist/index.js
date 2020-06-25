a6a2716cc69632cb3d821e7a1b0f9466640e4532
Altin Ukshini <altin.ukshini@gmail.com>
2020-06-25 02:13:18 +0200

    Packaging

--- dist/index.js
+++ dist/index.js
@@ -3207,10 +3207,16 @@ function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
             const context = github.context;
-            const logUrl = `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
+            const prStringInput = core.getInput("pr", {
+                required: false
+            });
+            const pr = prStringInput === "true";
+            const pr_id = core.getInput("pr_id", { required: false }) || 0;
+            const logUrl = pr ? `https://github.com/${context.repo.owner}/${context.repo.repo}/pull/${pr_id}/checks` : `https://github.com/${context.repo.owner}/${context.repo.repo}/commit/${context.sha}/checks`;
             const token = core.getInput("token", { required: true });
             const ref = core.getInput("ref", { required: false }) || context.ref;
             const url = core.getInput("target_url", { required: false }) || logUrl;
+            const payload = `{"web_url": "${url}"}`;
             const environment = core.getInput("environment", { required: false }) || "production";
             const description = core.getInput("description", { required: false });
             const initialStatus = core.getInput("initial_status", {
@@ -3219,19 +3225,24 @@ function run() {
             const autoMergeStringInput = core.getInput("auto_merge", {
                 required: false
             });
+            const transientEnvironmentStringInput = core.getInput("transient_environment", {
+                required: false
+            });
             const auto_merge = autoMergeStringInput === "true";
+            const transient_environment = transientEnvironmentStringInput === "true";
             const client = new github.GitHub(token, { previews: ["flash", "ant-man"] });
             const deployment = yield client.repos.createDeployment({
                 owner: context.repo.owner,
                 repo: context.repo.repo,
                 ref: ref,
+                payload,
                 required_contexts: [],
                 environment,
-                transient_environment: true,
+                transient_environment,
                 auto_merge,
                 description
             });
-            yield client.repos.createDeploymentStatus(Object.assign({}, context.repo, { deployment_id: deployment.data.id, state: initialStatus, log_url: logUrl, environment_url: url }));
+            yield client.repos.createDeploymentStatus(Object.assign({}, context.repo, { deployment_id: deployment.data.id, state: initialStatus, log_url: logUrl, environment_url: url, description }));
             core.setOutput("deployment_id", deployment.data.id.toString());
         }
         catch (error) {
